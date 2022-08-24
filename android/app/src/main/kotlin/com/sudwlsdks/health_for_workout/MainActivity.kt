f4716c0579f914a9d468dcd0033184fc4ed2967e
hi-jin <crushed7@o.cnu.ac.kr>
2022-08-24 18:45:02 +0900

    install libraries

---
+++ android/app/src/main/kotlin/com/sudwlsdks/health_for_workout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sudwlsdks.health_for_workout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:health_for_workout/view/intro.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Intro(),
+      home: Container(),
     );
   }
 }
