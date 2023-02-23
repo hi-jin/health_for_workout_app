c661bfbdb42ac9f2c0e954e0bde931a020de81de
hi-jin <crushed7@o.cnu.ac.kr>
2023-02-23 20:28:52 +0900

    INITIAL COMMIT

---
+++ android/app/src/main/kotlin/com/example/health_for_workout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.health_for_workout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class MyColors {
+  static final lightBlack = Color.fromRGBO(66, 66, 66, 1);
+  static final darkBlack = Color.fromRGBO(48, 48, 48, 1);
+}
