615568e07d665d7be71226c5ad41cff576a8d738
hi-jin <crushed7@o.cnu.ac.kr>
2023-03-08 14:26:50 +0900

    Refactor: SwitchButton

--- lib/ui/workout/workout_settings_screen.dart
+++ lib/ui/workout/workout_settings_screen.dart
@@ -13,12 +13,31 @@ class WorkoutSettingsScreen extends StatefulWidget {
 }
 
 class _WorkoutSettingsScreenState extends State<WorkoutSettingsScreen> {
-  final minutesController = TextEditingController();
-  final secondsController = TextEditingController();
+  final _minutesController = TextEditingController();
+  final _secondsController = TextEditingController();
+  final _timerSwitchWidgetController = _SwitchButtonWithDescController();
+  final _screentimeSwitchWidgetController = _SwitchButtonWithDescController();
+  final _breaktimeSwitchWidgetController = _SwitchButtonWithDescController();
 
-  bool timerSelected = false;
-  bool screentimeSelected = false;
-  bool breaktimeSelected = false;
+  bool _timerSelected = false;
+
+  void _timerSwitchControllerListenerCallback() {
+    setState(() {
+      _timerSelected = _timerSwitchWidgetController.selected;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _timerSwitchWidgetController.addListener(_timerSwitchControllerListenerCallback);
+  }
+
+  @override
+  void dispose() {
+    _timerSwitchWidgetController.removeListener(_timerSwitchControllerListenerCallback);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,149 +49,34 @@ class _WorkoutSettingsScreenState extends State<WorkoutSettingsScreen> {
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               const SizedBox(height: 30),
-              GestureDetector(
-                onTap: () => setState(() => timerSelected = !timerSelected),
-                child: FullWidthCardWidget(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(FontAwesomeIcons.clock),
-                                    const SizedBox(width: 10),
-                                    Expanded(child: Text('휴식 타이머 선택', style: TextStyle(fontSize: 18))),
-                                  ],
-                                ),
-                                const SizedBox(height: 5),
-                                Text('- 휴식 시간을 효율적으로 관리할 수 있습니다.'),
-                              ],
-                            ),
-                          ),
-                          Switch(
-                            value: timerSelected,
-                            onChanged: (val) {
-                              setState(() => timerSelected = val);
-                            },
-                          ),
-                        ],
-                      ),
-                      if (timerSelected) const SizedBox(height: 20),
-                      if (timerSelected)
-                        Row(
-                          children: [
-                            Text('휴식 시간 : ', style: TextStyle(fontSize: 18)),
-                            const SizedBox(width: 20),
-                            Expanded(
-                              child: TextField(
-                                controller: minutesController,
-                                textAlign: TextAlign.right,
-                                keyboardType: TextInputType.number,
-                                inputFormatters: [
-                                  FilteringTextInputFormatter.digitsOnly,
-                                ],
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(width: 10),
-                            Text('분', style: TextStyle(fontSize: 18)),
-                            const SizedBox(width: 20),
-                            Expanded(
-                              child: TextField(
-                                controller: secondsController,
-                                textAlign: TextAlign.right,
-                                keyboardType: TextInputType.number,
-                                inputFormatters: [
-                                  FilteringTextInputFormatter.digitsOnly,
-                                ],
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(width: 10),
-                            Text('초', style: TextStyle(fontSize: 18)),
-                          ],
-                        ),
-                    ],
-                  ),
+              _SwitchButtonWithDesc(
+                controller: _timerSwitchWidgetController,
+                icon: const Icon(FontAwesomeIcons.clock),
+                title: '휴식 타이머 선택',
+                subTitles: ['- 휴식 시간을 효율적으로 관리할 수 있습니다.'],
+              ),
+              if (_timerSelected)
+                _TimerDetailInputWidget(
+                  text: '휴식 시간',
+                  minutesController: _minutesController,
+                  secondsController: _secondsController,
                 ),
+              const SizedBox(height: 20),
+              _SwitchButtonWithDesc(
+                controller: _screentimeSwitchWidgetController,
+                icon: const Icon(FontAwesomeIcons.mobileScreen),
+                title: '스크린타임 기록하기',
+                subTitles: ['- 운동에 온전히 집중한 시간을 기록합니다.'],
               ),
               const SizedBox(height: 20),
-              GestureDetector(
-                onTap: () => setState(() => screentimeSelected =!screentimeSelected),
-                child: FullWidthCardWidget(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(FontAwesomeIcons.mobileScreen),
-                                const SizedBox(width: 10),
-                                Expanded(child: Text('스크린타임 기록하기', style: TextStyle(fontSize: 18))),
-                              ],
-                            ),
-                            const SizedBox(height: 5),
-                            Text('- 운동에 온전히 집중한 시간을 기록합니다.'),
-                          ],
-                        ),
-                      ),
-                      Switch(
-                        value: screentimeSelected,
-                        onChanged: (val) {
-                          setState(() => screentimeSelected = val);
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              GestureDetector(
-                onTap: () => setState(() => breaktimeSelected =!breaktimeSelected),
-                child: FullWidthCardWidget(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(FontAwesomeIcons.userClock),
-                                const SizedBox(width: 15),
-                                Expanded(child: Text('휴식 시간 기록하기', style: TextStyle(fontSize: 18))),
-                              ],
-                            ),
-                            const SizedBox(height: 5),
-                            Text('- 휴식한 시간을 운동 기록에 저장합니다.'),
-                            Text('- \'통계 보기\' 메뉴에서 확인할 수 있습니다.'),
-                          ],
-                        ),
-                      ),
-                      Switch(
-                        value: breaktimeSelected,
-                        onChanged: (val) {
-                          setState(() => breaktimeSelected = val);
-                        },
-                      ),
-                    ],
-                  ),
-                ),
+              _SwitchButtonWithDesc(
+                controller: _breaktimeSwitchWidgetController,
+                icon: const Icon(FontAwesomeIcons.userClock),
+                title: '휴식 시간 기록하기',
+                subTitles: [
+                  '- 휴식한 시간을 운동 기록에 저장합니다.',
+                  '- \'통계 보기\' 메뉴에서 확인할 수 있습니다.',
+                ],
               ),
               const SizedBox(height: 40),
               Padding(
@@ -193,6 +97,152 @@ class _WorkoutSettingsScreenState extends State<WorkoutSettingsScreen> {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TimerDetailInputWidget extends StatelessWidget {
+  final String text;
+  final TextEditingController minutesController;
+  final TextEditingController secondsController;
+
+  const _TimerDetailInputWidget({
+    super.key,
+    required this.text,
+    required this.minutesController,
+    required this.secondsController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FullWidthCardWidget(
+      child: Row(
+        children: [
+          Text('${text} : ', style: TextStyle(fontSize: 18)),
+          const SizedBox(width: 20),
+          Expanded(
+            child: TextField(
+              controller: minutesController,
+              textAlign: TextAlign.right,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Text('분', style: TextStyle(fontSize: 18)),
+          const SizedBox(width: 20),
+          Expanded(
+            child: TextField(
+              controller: secondsController,
+              textAlign: TextAlign.right,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Text('초', style: TextStyle(fontSize: 18)),
+        ],
+      ),
+    );
+  }
+}
+
+class _SwitchButtonWithDescController extends ChangeNotifier {
+  bool _selected = false;
+
+  _SwitchButtonWithDescController() : super();
+
+  bool get selected => _selected;
+
+  void toggleSelectedState() {
+    _selected = !_selected;
+    notifyListeners();
+  }
+}
+
+class _SwitchButtonWithDesc extends StatefulWidget {
+  const _SwitchButtonWithDesc({
+    super.key,
+    required this.controller,
+    required this.icon,
+    required this.title,
+    required this.subTitles,
+  });
+
+  final _SwitchButtonWithDescController controller;
+  final Widget icon;
+  final String title;
+  final List<String> subTitles;
+
+  @override
+  State<_SwitchButtonWithDesc> createState() => __SwitchButtonWithDescState();
+}
+
+class __SwitchButtonWithDescState extends State<_SwitchButtonWithDesc> {
+  late bool _selected;
+
+  void _controllerListenerCallback() {
+    setState(() {
+      _selected = widget.controller.selected;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _selected = widget.controller.selected;
+    widget.controller.addListener(_controllerListenerCallback);
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(_controllerListenerCallback);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => widget.controller.toggleSelectedState(),
+      child: FullWidthCardWidget(
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      widget.icon,
+                      const SizedBox(width: 15),
+                      Expanded(child: Text(widget.title, style: TextStyle(fontSize: 18))),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  ...widget.subTitles.map((subTitle) => Text(subTitle)),
+                ],
+              ),
+            ),
+            Switch(
+              value: _selected,
+              onChanged: (val) => widget.controller.toggleSelectedState(),
+            ),
+          ],
         ),
       ),
     );
